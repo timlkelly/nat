613d363020115e60980e5ae6f114462cf5f24c7d
timlkelly <timlkelly@gmail.com>
2017-04-07 12:36:53 -0500

    Separate snacks by type

--- app/controllers/snacks_controller.rb
+++ app/controllers/snacks_controller.rb
@@ -1,6 +1,8 @@
 class SnacksController < ApplicationController
   def index
     @title = 'Nerdery OCD Snacks'
-    @snacks = Snack.all
+
+    @permanent_snacks = Snack.where(optional: false)
+    @suggested_snacks = Snack.where(optional: true)
   end
 end
--- spec/controllers/snacks_controller_spec.rb
+++ spec/controllers/snacks_controller_spec.rb
@@ -2,14 +2,16 @@ require 'rails_helper'
 
 RSpec.describe SnacksController, type: :controller do
   describe 'GET index' do
-    let(:snack) { FactoryGirl.create(:snack) }
+    let(:permanent_snack) { FactoryGirl.create(:snack, optional: false) }
+    let(:suggested_snack) { FactoryGirl.create(:snack, optional: true) }
 
     it 'renders 200 and assigns @snacks' do
       get :index
 
       expect(response.status).to eq(200)
       expect(response).to render_template('index')
-      expect(assigns(:snacks)).to eq([snack])
+      expect(assigns(:permanent_snacks)).to eq([permanent_snack])
+      expect(assigns(:suggested_snacks)).to eq([suggested_snack])
     end
   end
 end
