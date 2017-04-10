b951901ccf66c3f061499d37c14d3e140b0c912f
timlkelly <timlkelly@gmail.com>
2017-04-10 13:10:08 -0500

    Sanitize snack name

--- app/models/snack.rb
+++ app/models/snack.rb
@@ -1,4 +1,9 @@
 class Snack < ApplicationRecord
   validates_presence_of :name, :purchase_location
   validates_uniqueness_of :name
+  before_validation :downcase_name
+
+  def downcase_name
+    self.name.present? && self.name.downcase!
+  end
 end
--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Snack, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:purchase_location) }
-    it { should validate_uniqueness_of(:name) }
+    it { should validate_uniqueness_of(:name).ignoring_case_sensitivity }
   end
 end
